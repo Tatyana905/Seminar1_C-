8e586e7fdb77fb33ac27e6914e7b50a62da251b4
Tatyana905 <905478@mail.ru>
2022-08-18 17:08:19 +0300

    001

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿/*написать  программу, к-рая возводит число в квадрат
+например 4-> 16. -3 -> 9
+*/
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+//5^2 = 5*5
+
+int square = number * number;
+
+/*camelCase
+double testResult = Math.Pow(number, 2); 
+Pow - сокращение от power (число, степень) */
+
+Console.WriteLine("Результат: " + square);
+//конкатенация - к левой строчке приписывается правая
